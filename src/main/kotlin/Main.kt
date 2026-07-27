bb0fd64b6c9d93df68292027d45b915b00537ff1
Geming400 <Geming400@gmail.com>
2026-07-28 00:10:45 +0200

    Added todo removal on main function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,11 @@ package fr.geming400.gddotkt
 
 import fr.geming400.gddotkt.objects.SimpleObject
 import fr.geming400.gddotkt.objects.data.GridPos
+import fr.geming400.gddotkt.objects.data.Hsv
 
-fun main() {
+// TODO: Once this is actually more closer to being finished delete this temporary main function
+private fun main() {
     val obj = SimpleObject(1u, GridPos.gridCentered(0f, 0f))
+    obj.baseColorHSV.value = Hsv.checkedBrightness()
     println("Raw string is: ${obj.asRawString()}")
 }
