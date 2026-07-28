8f8143a4b078ac59ebf6a0f413100949de364ec9
Geming400 <Geming400@gmail.com>
2026-07-29 00:43:23 +0200

    Added some new trigger properties

--- src/main/kotlin/objects/propertycontainers/TriggerProperties.kt
+++ src/main/kotlin/objects/propertycontainers/TriggerProperties.kt
@@ -4,22 +4,32 @@ import fr.geming400.gddotkt.objects.data.enums.Easing
 import fr.geming400.gddotkt.rawstring.id
 import fr.geming400.gddotkt.rawstring.property.EnumProperty
 import fr.geming400.gddotkt.rawstring.property.FloatProperty
+import fr.geming400.gddotkt.rawstring.property.GDDurationProperty
+import fr.geming400.gddotkt.rawstring.property.HsvProperty
 import fr.geming400.gddotkt.rawstring.property.MutableConditionalProperty
+import fr.geming400.gddotkt.rawstring.property.RangedFloatProperty
 import fr.geming400.gddotkt.rawstring.property.UIntProperty
 import fr.geming400.gddotkt.rawstring.serializing.Serializer
 
 object TriggerProperties {
-    private val EASING_RATE_RANGE = 0.1f..20f
+    val EASING_RATE_RANGE = 0.1f..20f
+    const val MAX_GROUPS = 9999u
 
 
-    inline val DURATION: FloatProperty
-        get() = FloatProperty(10.id)
+    inline val DURATION: GDDurationProperty
+        get() = GDDurationProperty(10.id)
 
     inline val EASING: EnumProperty<Easing>
         get() = EnumProperty(30.id, Serializer.enum(Easing.entries))
 
     inline val TARGET_GROUP: UIntProperty
-        get() = UIntProperty(51.id)
+        get() = UIntProperty.ranged(51.id, 1u..MAX_GROUPS)
+
+    inline val OPACITY: RangedFloatProperty
+        get() = FloatProperty.ranged(35.id, 0f..1f)
+
+    inline val HSV: HsvProperty
+        get() = HsvProperty(49.id)
 
     fun getEasingRateProp(easingProp: EnumProperty<Easing>) =
         MutableConditionalProperty(85.id, 0f, dependantOn = easingProp, valueChanger = { it.coerceIn(EASING_RATE_RANGE) }, serializer = Serializer.clampedFloat(EASING_RATE_RANGE)) {
--- src/main/kotlin/objects/triggers/AlphaTrigger.kt
+++ src/main/kotlin/objects/triggers/AlphaTrigger.kt
@@ -1,9 +1,8 @@
 package fr.geming400.gddotkt.objects.triggers
 
+import fr.geming400.gddotkt.annotations.GDName
 import fr.geming400.gddotkt.objects.data.Position
 import fr.geming400.gddotkt.objects.propertycontainers.TriggerProperties
-import fr.geming400.gddotkt.rawstring.id
-import fr.geming400.gddotkt.rawstring.property.FloatProperty
 
 class AlphaTrigger : TriggerObject {
     companion object {
@@ -11,8 +10,9 @@ class AlphaTrigger : TriggerObject {
     }
 
     val fadeTime = TriggerProperties.DURATION
+    @GDName("Spawn Trigger")
     val targetGroup = TriggerProperties.TARGET_GROUP
-    val opacity = FloatProperty.ranged(35.id, 0f..1f)
+    val opacity = TriggerProperties.OPACITY
 
     constructor(pos: Position) : super(OBJ_ID, pos)
     constructor(x: Float, y: Float) : super(OBJ_ID, x, y)
