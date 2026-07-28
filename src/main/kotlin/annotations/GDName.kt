ae3901dabf0726d7fda8088fd4e35d1bfead22a4
Geming400 <Geming400@gmail.com>
2026-07-28 23:55:31 +0200

    Added clarification on the usage of the GDName annotation

--- src/main/kotlin/annotations/GDName.kt
+++ src/main/kotlin/annotations/GDName.kt
@@ -4,7 +4,11 @@ package fr.geming400.gddotkt.annotations
  * Represents the actual name of the property in geometry dash. Some properties don't have names (eg: obj id)
  * but some have, like in the `extra` tab.
  *
- * Spaces are allowed in the [name]
+ * This is mostly useful when a property's name was renamed
+ * in the code to make more sense, while in geometry dash
+ * it might be more ambiguous.
+ *
+ * Spaces are allowed in the [name], this is intended behavior.
  * @property name the name in geometry dash
  */
 @Target(AnnotationTarget.PROPERTY, AnnotationTarget.FIELD)
