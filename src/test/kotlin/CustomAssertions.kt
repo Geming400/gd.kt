d0818ad501a301bdc119716797ee412d76575a93
Geming400 <Geming400@gmail.com>
2026-07-28 14:20:21 +0200

    Added custom assertions for raw strings equality

---
+++ src/test/kotlin/CustomAssertions.kt
@@ -0,0 +1,18 @@
+package fr.geming400.gddotkt
+
+import fr.geming400.gddotkt.rawstring.RawStringFactory
+import org.opentest4j.AssertionFailedError
+
+object CustomAssertions {
+    fun assertRawStringEquals(expected: String, actual: String) {
+        val res = RawStringFactory.areRawStringEquals(expected, actual)
+        if (!res)
+            throw AssertionFailedError("Raw strings are different", expected, actual)
+    }
+
+    fun assertRawStringNotEquals(unexpected: String, actual: String) {
+        val res = RawStringFactory.areRawStringEquals(unexpected, actual)
+        if (res)
+            throw AssertionFailedError("Raw strings are equal", unexpected, actual)
+    }
+}
