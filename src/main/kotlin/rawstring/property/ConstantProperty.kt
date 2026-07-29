5892c3931333779a259b20b3979360617ab99b26
Geming400 <Geming400@gmail.com>
2026-07-30 00:14:23 +0200

    Made ConstantProperty actually immutable / constant

--- src/main/kotlin/rawstring/property/ConstantProperty.kt
+++ src/main/kotlin/rawstring/property/ConstantProperty.kt
@@ -6,9 +6,12 @@ import fr.geming400.gddotkt.rawstring.serializing.Serializer
 /**
  * A constant property is **always** serializable and serializes to a **[constant value][constantValue]**
  */
-class ConstantProperty<T>(id: Id, constantValue: T, override val serializer: Serializer<T>) : AbstractProperty<T>(id, constantValue, constantValue) {
+class ConstantProperty<T>(override val id: Id, val constantValue: T, override val serializer: Serializer<T>) : PropertyDefinition<T> {
+    override val value: T?
+        get() = this.constantValue
+
     override fun isSerializable(): Boolean = true
 
     override fun toRawString(): String =
-        this.toRawStringHelper(this.serializer)
+        this.id.getID() + AbstractProperty.KEY_VAL_SEPARATOR + this.serializer.serialize(this.constantValue)
 }
