b6285dfdefcd15f59b92e856195f5354561711ab
Geming400 <Geming400@gmail.com>
2026-07-30 00:20:03 +0200

    Added move triggers

--- src/main/kotlin/objects/triggers/TriggerObject.kt
+++ src/main/kotlin/objects/triggers/TriggerObject.kt
@@ -4,17 +4,8 @@ import fr.geming400.gddotkt.objects.ComplexObject
 import fr.geming400.gddotkt.objects.data.Position
 import fr.geming400.gddotkt.rawstring.id
 import fr.geming400.gddotkt.rawstring.property.BoolProperty
-import fr.geming400.gddotkt.rawstring.property.IndependentMutableConditionalProperty
 import fr.geming400.gddotkt.rawstring.property.MutableConditionalProperty
 import fr.geming400.gddotkt.rawstring.serializing.Serializers
-
-/*
-TODO: Triggers that needs to be made:
-- [x] alpha trigger
-- [x] toggle trigger
-- move trigger
-- [x] color trigger
- */
 
 /**
  * A trigger object is the base class for every trigger.
@@ -24,7 +15,7 @@ TODO: Triggers that needs to be made:
 abstract class TriggerObject : ComplexObject {
     val spawnTriggered = BoolProperty(62.id, false)
     val touchTriggered = BoolProperty(11.id, false)
-    val multiTriggered: IndependentMutableConditionalProperty<Boolean> =
+    val multiTriggered =
         MutableConditionalProperty.createIndependent(87.id, defaultValue = false, serializer = Serializers.BOOLEAN) {
             this@TriggerObject.spawnTriggered.isSerializable() || this@TriggerObject.touchTriggered.isSerializable()
         }
