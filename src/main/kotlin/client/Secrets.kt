3ac6bd383f10cd75a4defa16581d315fdcb2dba3
Geming400 <Geming400@gmail.com>
2026-08-15 23:05:42 +0200

    Added a class to store gd's secrets

---
+++ src/main/kotlin/client/Secrets.kt
@@ -0,0 +1,12 @@
+package fr.geming400.gddotkt.client
+
+/**
+ * A list of secrets used for requests
+ * @see GDClient
+ */
+object Secrets {
+    const val COMMON: String = "Wmfd2893gb7"
+    const val ACCOUNT: String = "Wmfv3899gc9"
+    const val LEVEL: String = "Wmfv2898gc9"
+    const val MOD: String = "Wmfp3879gc3"
+}
