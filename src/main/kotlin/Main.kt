73eb5140ab049844364fbad820b842163188cc85
Geming400 <Geming400@gmail.com>
2026-08-16 00:36:41 +0200

    Added todo for me

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,7 @@ import fr.geming400.gddotkt.editor.objects.data.GridPos
 import fr.geming400.gddotkt.editor.objects.data.Hsv
 
 // TODO: Once this is actually more closer to being finished delete this temporary main function
+// TODO: Store sentitive infos in system properties
 private fun main() {
     val obj = SimpleObject(1u, GridPos.gridCentered(0f, 0f))
     obj.baseColorHSV.value = Hsv.checkedBrightness()
