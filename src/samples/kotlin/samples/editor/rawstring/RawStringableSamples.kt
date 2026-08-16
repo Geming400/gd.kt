d28f96f9e6701aaa6147a956df607733a7ae0593
Geming400 <Geming400@gmail.com>
2026-08-16 13:44:11 +0200

    fixed RawStringableSamples.isValidObjectStringSample being wrong

--- src/samples/kotlin/samples/editor/rawstring/RawStringableSamples.kt
+++ src/samples/kotlin/samples/editor/rawstring/RawStringableSamples.kt
@@ -1,14 +1,15 @@
 package samples.editor.rawstring
 
 import editor.objects.GenericGdObject
+import editor.rawstring.property.AbstractProperty
 
 private fun isValidObjectStringSample() {
     // This will return true
     // In its form it's a valid raw string
-    GenericGdObject.isValidObjectString("1,1" + "2,2" + "3,3")
+    GenericGdObject.isValidObjectString(listOf("1,1", "2,2", "3,3").joinToString(AbstractProperty.KEY_VAL_SEPARATOR.toString()))
 
     // However this will return false
     // In its form it's NOT a valid raw string because it has an even
     // number of commas
-    GenericGdObject.isValidObjectString("1,1" + "2,2" + "3,3" + "4")
+    GenericGdObject.isValidObjectString(listOf("1,1", "2,2", "3,3", "4").joinToString(AbstractProperty.KEY_VAL_SEPARATOR.toString()))
 }
