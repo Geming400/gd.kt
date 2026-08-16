bef216997d35bddfdcda9f56df02b577a2648745
Geming400 <Geming400@gmail.com>
2026-08-16 13:17:20 +0200

    Added other implementations for ObjectParser

--- src/main/kotlin/editor/objects/ObjectParser.kt
+++ src/main/kotlin/editor/objects/ObjectParser.kt
@@ -13,7 +13,28 @@ import kotlin.reflect.full.starProjectedType
 object ObjectParser {
     /**
      * Parses any raw string into [T]. Every [mutable properties][MutableProperty] of this object
-     * are filled accordingly to the raw string
+     * are filled accordingly to the raw string.
+     * This chooses which parsing implementation to use
+     * @param rawString the raw string to parse
+     * @param toFill the instance to fill
+     * @param separator the separator used to separate the raw string (ex, in `1,10,2,20,3,30` the separator is `,`)
+     * @return returns the final instance, aka the same as [toFill]
+     * @throws exceptions.InvalidRawStringException if the raw string is invalid *(see [GenericGdObject.isValidObjectString])*
+     * @throws IllegalArgumentException if any of the parsed [ids][Id] are below `0` (exclusive, so `< 0`)
+     * @see parseAny
+     * @see parseGdObject
+     */
+    inline fun <reified T : Any> parse(rawString: String, toFill: T = T::class.createInstance(), separator: Char = AbstractProperty.KEY_VAL_SEPARATOR): T {
+        return if (T::class.isInstance(GenericGdObject::class))
+            parseGdObject(rawString, toFill as GenericGdObject, separator) as T
+        else
+            parseAny(rawString, toFill, separator)
+    }
+
+    /**
+     * Parses any raw string into [T]. Every [mutable properties][MutableProperty] of this object
+     * are filled accordingly to the raw string.
+     * To parse a [GenericGdObject], [parseGdObject] should get used instead
      * @param rawString the raw string to parse
      * @param toFill the instance to fill
      * @param separator the separator used to separate the raw string (ex, in `1,10,2,20,3,30` the separator is `,`)
@@ -21,7 +42,7 @@ object ObjectParser {
      * @throws exceptions.InvalidRawStringException if the raw string is invalid *(see [GenericGdObject.isValidObjectString])*
      * @throws IllegalArgumentException if any of the parsed [ids][Id] are below `0` (exclusive, so `< 0`)
      */
-    inline fun <reified T : Any> parse(rawString: String, toFill: T = T::class.createInstance(), separator: Char = AbstractProperty.KEY_VAL_SEPARATOR): T {
+    inline fun <reified T : Any> parseAny(rawString: String, toFill: T = T::class.createInstance(), separator: Char = AbstractProperty.KEY_VAL_SEPARATOR): T {
         val rawStringMap = RawStringFactory.rawStringToMap(rawString, separator)
         T::class.memberProperties.forEach {
             if (it.visibility == KVisibility.PUBLIC && it.returnType.isSubtypeOf(MutableProperty::class.starProjectedType)) {
@@ -33,6 +54,32 @@ object ObjectParser {
                     if (value != null)
                         prop.value = value
                 }
+            }
+        }
+
+        return toFill
+    }
+
+    /**
+     * Parses any raw string into [T]. Every [mutable properties][MutableProperty] of this object
+     * are filled accordingly to the raw string. This uses the obj's [RawStringFactory] to iterate through the properties, using its implementation instead.
+     * To parse something that is NOT a [GenericGdObject], [parseAny] should get used instead
+     * @param rawString the raw string to parse
+     * @param toFill the instance to fill
+     * @param separator the separator used to separate the raw string (ex, in `1,10,2,20,3,30` the separator is `,`)
+     * @return returns the final instance, aka the same as [toFill]
+     * @throws exceptions.InvalidRawStringException if the raw string is invalid *(see [GenericGdObject.isValidObjectString])*
+     * @throws IllegalArgumentException if any of the parsed [ids][Id] are below `0` (exclusive, so `< 0`)
+     */
+    inline fun <reified T : GenericGdObject> parseGdObject(rawString: String, toFill: T = T::class.createInstance(), separator: Char = AbstractProperty.KEY_VAL_SEPARATOR): T {
+        val rawStringMap = RawStringFactory.rawStringToMap(rawString, separator)
+        toFill.rawStringFactory.properties.forEach {
+            if (rawStringMap.containsKey(it.id) && it is MutableProperty<*>) {
+                @Suppress("UNCHECKED_CAST")
+                val castedProp = it as MutableProperty<Any?>
+                val value = castedProp.serializer.parse(rawStringMap[castedProp.id]!!)
+                if (value != null)
+                    castedProp.value = value
             }
         }
 
--- src/test/kotlin/editor/objects/ObjectParserTests.kt
+++ src/test/kotlin/editor/objects/ObjectParserTests.kt
@@ -1,20 +1,98 @@
 package editor.objects
 
 import TestTags
+import editor.rawstring.id
+import editor.rawstring.property.IntProperty
+import editor.rawstring.property.ListProperty
+import editor.rawstring.serializing.Serializers
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+
+class NotGenericObject {
+    val intProperty = IntProperty(1.id, defaultValue = 5)
+    val listProperty = ListProperty(2.id, elemSerializer = Serializers.INT, defaultValue = mutableListOf(5, 4, 3, 2))
+
+    fun asRawString(): String =
+        "${intProperty.toRawString()},${listProperty.toRawString()}"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is NotGenericObject) return false
+
+        if (this.intProperty.value != other.intProperty.value) return false
+        if (this.listProperty.value != other.listProperty.value) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = this.intProperty.hashCode()
+        result = 31 * result + this.listProperty.hashCode()
+        return result
+    }
+}
 
 @Tag(TestTags.EDITOR)
 private class ObjectParserTests {
     @Test
-    fun objectParserTest() {
+    @DisplayName("ObjectParser.parse (GenericGDObject) Test")
+    fun objectParserTestGeneric() {
         val obj = SimpleObject(2u, 0f, 0f)
         var parsedObj = ObjectParser.parse(obj.asRawString(), SimpleObject(0u, 0f, 0f))
         Assertions.assertEquals(obj, parsedObj)
 
         obj.linkedGroupID.value = 5
         parsedObj = ObjectParser.parse(SimpleObject(2u, 0f, 0f).asRawString(), SimpleObject(0u, 0f, 0f))
+        Assertions.assertNotEquals(obj, parsedObj)
+    }
+
+    @Test
+    @DisplayName("ObjectParser.parseGdObject (GenericGDObject) Test")
+    fun objectParserTestGenericObject() {
+        val obj = SimpleObject(2u, 0f, 0f)
+        var parsedObj = ObjectParser.parseGdObject(obj.asRawString(), SimpleObject(0u, 0f, 0f))
+        Assertions.assertEquals(obj, parsedObj)
+
+        obj.linkedGroupID.value = 5
+        parsedObj = ObjectParser.parseGdObject(SimpleObject(2u, 0f, 0f).asRawString(), SimpleObject(0u, 0f, 0f))
+        Assertions.assertNotEquals(obj, parsedObj)
+    }
+
+    @Test
+    @DisplayName("ObjectParser.parseAny (GenericGDObject) Test")
+    fun objectParserTestGenericAny() {
+        val obj = SimpleObject(2u, 0f, 0f)
+        var parsedObj = ObjectParser.parseAny(obj.asRawString(), SimpleObject(0u, 0f, 0f))
+        Assertions.assertEquals(obj, parsedObj)
+
+        obj.linkedGroupID.value = 5
+        parsedObj = ObjectParser.parseAny(SimpleObject(2u, 0f, 0f).asRawString(), SimpleObject(0u, 0f, 0f))
+        Assertions.assertNotEquals(obj, parsedObj)
+    }
+
+    @Test
+    @DisplayName("ObjectParser.parse (Any) Test")
+    fun objectParserTestAny() {
+        val obj = NotGenericObject()
+        var parsedObj = ObjectParser.parse(obj.asRawString(), NotGenericObject())
+        Assertions.assertEquals(obj, parsedObj)
+
+        obj.intProperty.value = 3
+        parsedObj = ObjectParser.parse(NotGenericObject().asRawString(), NotGenericObject())
+        Assertions.assertNotEquals(obj, parsedObj)
+    }
+
+    @Test
+    @DisplayName("ObjectParser.parseAny (Any) Test")
+    fun objectParserTestParseAny() {
+        val obj = NotGenericObject()
+        var parsedObj = ObjectParser.parseAny(obj.asRawString(), NotGenericObject())
+        Assertions.assertEquals(obj, parsedObj)
+
+        obj.intProperty.value = 3
+        parsedObj = ObjectParser.parseAny(NotGenericObject().asRawString(), NotGenericObject())
         Assertions.assertNotEquals(obj, parsedObj)
     }
 }
