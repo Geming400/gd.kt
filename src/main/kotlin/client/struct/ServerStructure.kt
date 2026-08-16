23f540ebf74763f9955f75bc9950cf53bf7f3d8e
Geming400 <Geming400@gmail.com>
2026-08-16 14:46:03 +0200

    Added kdoc to ServerStructure.asRawString

--- src/main/kotlin/client/struct/ServerStructure.kt
+++ src/main/kotlin/client/struct/ServerStructure.kt
@@ -5,4 +5,12 @@ import editor.objects.GenericGdObject
 /**
  * Represents a structure that can be returned by Robtop's server.
  */
-interface ServerStructure : GenericGdObject
+interface ServerStructure : GenericGdObject {
+    /**
+     * Get the geometry dash raw string representing this object.
+     * Generally on server structures **there's no need to use this**
+     * @return the raw string representing this object
+     */
+    override fun asRawString(): String =
+        super.asRawString()
+}
