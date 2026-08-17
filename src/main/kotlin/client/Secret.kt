e1cf2a8b62583120d0706fdef5f701949cd3f2b5
Geming400 <Geming400@gmail.com>
2026-08-17 22:12:39 +0200

    Turned "Secrets" object into an Enum

---
+++ src/main/kotlin/client/Secret.kt
@@ -0,0 +1,12 @@
+package client
+
+/**
+ * A list of secrets used for requests
+ * @see GDClient
+ */
+enum class Secret(val secret: String) {
+    COMMON("Wmfd2893gb7"),
+    ACCOUNT("Wmfv3899gc9"),
+    LEVEL("Wmfv2898gc9"),
+    MOD("Wmfp3879gc3")
+}
