9511a903373d18cf41e1560261eb13a41da610be
Geming400 <Geming400@gmail.com>
2026-09-09 16:13:43 +0200

    Updated object parser for dynamic raw string factory support Also added java interop, but this is a side effect of not wanting to have huge inline functions

--- src/main/kotlin/editor/objects/ObjectParser.kt
+++ src/main/kotlin/editor/objects/ObjectParser.kt
@@ -1,5 +1,6 @@
 package editor.objects
 
+import editor.rawstring.DynamicRawStringFactory
 import editor.rawstring.Id
 import editor.rawstring.RawStringFactory
 import editor.rawstring.property.AbstractProperty
@@ -42,9 +43,15 @@ object ObjectParser {
      * @throws exceptions.InvalidRawStringException if the raw string is invalid *(see [GenericGdObject.isValidObjectString])*
      * @throws IllegalArgumentException if any of the parsed [ids][Id] are below `0` (exclusive, so `< 0`)
      */
-    inline fun <reified T : Any> parseAny(rawString: String, toFill: T = T::class.createInstance(), separator: Char = AbstractProperty.KEY_VAL_SEPARATOR): T {
+    inline fun <reified T : Any> parseAny(rawString: String, toFill: T = T::class.createInstance(), separator: Char = AbstractProperty.KEY_VAL_SEPARATOR): T =
+        parseAnyJava(rawString, toFill, T::class.java, separator)
+
+    /**
+     * @see parseAnyJava
+     */
+    fun <T : Any> parseAnyJava(rawString: String, toFill: T, clazz: Class<T>, separator: Char = AbstractProperty.KEY_VAL_SEPARATOR): T {
         val rawStringMap = RawStringFactory.rawStringToMap(rawString, separator)
-        T::class.memberProperties.forEach {
+        clazz.kotlin.memberProperties.forEach {
             if (it.visibility == KVisibility.PUBLIC && it.returnType.isSubtypeOf(MutableProperty::class.starProjectedType)) {
                 @Suppress("UNCHECKED_CAST")
                 val prop = it.get(toFill) as MutableProperty<Any?>
@@ -63,7 +70,10 @@ object ObjectParser {
     /**
      * Parses any raw string into [T]. Every [mutable properties][MutableProperty] of this object
      * are filled accordingly to the raw string. This uses the obj's [RawStringFactory] to iterate through the properties, using its implementation instead.
-     * To parse something that is NOT a [GenericGdObject], [parseAny] should get used instead
+     * To parse something that is NOT a [GenericGdObject], [parseAny] should get used instead.
+     *
+     * If the [object][GenericGdObject]'s [RawStringFactory] is [dynamic][DynamicRawStringFactory], the properties not found inside
+     * the object will get added in the [dynamic properties][DynamicRawStringFactory.dynamicProperties] list
      * @param rawString the raw string to parse
      * @param toFill the instance to fill
      * @param separator the separator used to separate the raw string (ex, in `1,10,2,20,3,30` the separator is `,`)
@@ -71,15 +81,35 @@ object ObjectParser {
      * @throws exceptions.InvalidRawStringException if the raw string is invalid *(see [GenericGdObject.isValidObjectString])*
      * @throws IllegalArgumentException if any of the parsed [ids][Id] are below `0` (exclusive, so `< 0`)
      */
-    inline fun <reified T : GenericGdObject> parseGdObject(rawString: String, toFill: T = T::class.createInstance(), separator: Char = AbstractProperty.KEY_VAL_SEPARATOR): T {
+    inline fun <reified T : GenericGdObject> parseGdObject(rawString: String, toFill: T = T::class.createInstance(), separator: Char = AbstractProperty.KEY_VAL_SEPARATOR): T =
+        parseGdObjectJava(rawString, toFill, separator)
+
+    /**
+     * @see parseGdObject
+     */
+    fun <T : GenericGdObject> parseGdObjectJava(rawString: String, toFill: T, separator: Char = AbstractProperty.KEY_VAL_SEPARATOR): T {
         val rawStringMap = RawStringFactory.rawStringToMap(rawString, separator)
+        val isDynamicRawStringFactory = toFill.rawStringFactory is DynamicRawStringFactory
+
+        val defaultParseBehavior = { prop: MutableProperty<*> ->
+            @Suppress("UNCHECKED_CAST")
+            val castedProp = prop as MutableProperty<Any?>
+            val value = castedProp.serializer.parse(rawStringMap[castedProp.id]!!)
+            if (value != null)
+                castedProp.value = value
+        }
+
         toFill.rawStringFactory.properties.forEach {
-            if (rawStringMap.containsKey(it.id) && it is MutableProperty<*>) {
-                @Suppress("UNCHECKED_CAST")
-                val castedProp = it as MutableProperty<Any?>
-                val value = castedProp.serializer.parse(rawStringMap[castedProp.id]!!)
-                if (value != null)
-                    castedProp.value = value
+            if (it is MutableProperty<*>) {
+                if (isDynamicRawStringFactory) {
+                    if (rawStringMap.containsKey(it.id)) {
+                        defaultParseBehavior(it)
+                    } else {
+                        (toFill.rawStringFactory as DynamicRawStringFactory).dynamicProperties.add(it)
+                    }
+                } else if (rawStringMap.containsKey(it.id)) {
+                    defaultParseBehavior(it)
+                }
             }
         }
 
