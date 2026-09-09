d68dec1d408e0d7b8eb1643276bf031aacde521c
Geming400 <Geming400@gmail.com>
2026-09-09 17:57:37 +0200

    Now forcing server structures to contain a reference to a GDClient object

--- src/main/kotlin/client/struct/ServerStructure.kt
+++ src/main/kotlin/client/struct/ServerStructure.kt
@@ -1,11 +1,14 @@
 package client.struct
 
+import client.GDClient
 import editor.objects.GenericGdObject
 
 /**
  * Represents a structure that can be returned by Robtop's server.
  */
 interface ServerStructure : GenericGdObject {
+    val client: GDClient
+
     /**
      * Get the geometry dash raw string representing this object.
      * Generally on server structures **there's no need to use this**
