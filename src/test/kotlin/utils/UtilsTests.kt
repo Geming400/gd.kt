5ad2da22f77f5f55650718e86bae7eab69bbfeb0
Geming400 <Geming400@gmail.com>
2026-09-18 18:52:23 +0200

    Added tests for xor related functions

--- src/test/kotlin/utils/UtilsTests.kt
+++ src/test/kotlin/utils/UtilsTests.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import kotlin.random.Random
 
 private class UtilsTest {
     @Test
@@ -14,17 +15,21 @@ private class UtilsTest {
         Assertions.assertTrue(isPrimitive(0))
         Assertions.assertTrue(isPrimitive(0.0f))
         Assertions.assertTrue(isPrimitive(0.0))
+        Assertions.assertTrue(isPrimitive(0u))
         Assertions.assertTrue(isPrimitive(0L))
         Assertions.assertTrue(isPrimitive(0.toByte()))
         Assertions.assertTrue(isPrimitive(0.toShort()))
         Assertions.assertTrue(isPrimitive(false))
 
+        // Random classes
         Assertions.assertFalse(isPrimitive(arrayOf<Any>()))
+        Assertions.assertFalse(isPrimitive(UtilsTest()))
+        Assertions.assertFalse(isPrimitive(Random))
     }
 
     @Test
-    @DisplayName("Map.toFormRequestBody Test")
     @Tag(TestTags.CLIENT)
+    @DisplayName("Map.toFormRequestBody Test")
     fun toFormRequestBodyTest() {
         val mapWithPrimitives = mapOf(
             Pair("hi", 0),
@@ -46,5 +51,45 @@ private class UtilsTest {
         )
 
         Assertions.assertThrows(IllegalTypeException::class.java) { mapWithNoPrimitives.toFormRequestBody() }
+    }
+
+    @Test
+    @DisplayName("String.staticXor test")
+    fun staticXorTest() {
+        val string = "Hi hello !!! This is a cool string which is very long......".repeat(3)
+        val key = Random.nextInt()
+
+        val encryptedString = string.staticXor(key)
+
+        Assertions.assertNotEquals(string, encryptedString)
+        Assertions.assertEquals(string, encryptedString.staticXor(key))
+        Assertions.assertNotEquals(string, encryptedString.cyclicXor(key))
+    }
+
+    @Test
+    @DisplayName("String.cyclicXor(Int) test")
+    fun cyclicXorIntKeyTest() {
+        val string = "Hi hello !!! This is a cool string which is very long...... (v2)".repeat(3)
+        val key = Random.nextInt()
+
+        val encryptedString = string.cyclicXor(key)
+
+        Assertions.assertNotEquals(string, encryptedString)
+        Assertions.assertEquals(string, encryptedString.cyclicXor(key))
+        Assertions.assertNotEquals(string, encryptedString.staticXor(key))
+    }
+
+    @Test
+    @DisplayName("String.cyclicXor(String) test")
+    fun cyclicXorStringKeyTest() {
+        val string = "Hi hello !!! This is a cool string which is very long...... (v2)".repeat(3)
+
+        val baseKey = "Wow look at this amazing key I just made. I hope no one will steal it (please don't, I have a family to feed !)"
+        val key = baseKey.drop(Random.nextInt(baseKey.length - 5))
+
+        val encryptedString = string.cyclicXor(key)
+
+        Assertions.assertNotEquals(string, encryptedString)
+        Assertions.assertEquals(string, encryptedString.cyclicXor(key))
     }
 }
